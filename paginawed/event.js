13bf566318ca781fcdee9ff96cc21b5eb9187039
Andreys-Gp <109758319+Andreys-Gp@users.noreply.github.com>
2023-02-05 11:44:07 -0500

    Add files via upload

---
+++ paginawed/event.js
@@ -0,0 +1,3 @@
+
+window.alert("Bienvenido pagina electronic ");
+window.print("lReprehenderit excepteur duis enim commodo labore commodo ad sint laboris ipsum quis mollit consequat. Nisi proident commodo incididunt ullamco. Fugiat cupidatat ullamco consequat reprehenderit officia duis consectetur nulla dolore eu veniam aute velit.");
